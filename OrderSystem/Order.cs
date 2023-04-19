ba53c8fd7abb1a5c890f2478f1ec06ea5281ea4f
Travis Smith <travis.r.smith@accenture.com>
2023-04-19 13:24:39 -0500

    adding passing test for sending email

--- OrderSystem/Order.cs
+++ OrderSystem/Order.cs
@@ -1,11 +1,15 @@
+using System.Net.Mail;
+
 namespace OrderSystem;
 
 public class Order
 {
     private int Quantity { get; }
+    private readonly IEmailService _emailService;
 
-    public Order(int quantity)
+    public Order(IEmailService emailService, int quantity)
     {
+        _emailService = emailService;
         Quantity = quantity;
     }
 
@@ -13,6 +17,7 @@ public class Order
     {
         // Order is submitted to database; a unique order number is assigned
         var newOrderNumber = 1;
+        _emailService.Send(EmailBuilder.Build(Quantity));
         return newOrderNumber;
     }
 }
--- OrderSystemTests/OrderTests.cs
+++ OrderSystemTests/OrderTests.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using System.Net.Mail;
+using Moq;
 using OrderSystem;
 using Xunit;
 
@@ -8,8 +11,24 @@ public class OrderTests
     [Fact]
     public void OrderNumberIsAssigned_WhenOrderIsSubmitted()
     {
-        var order = new Order(25);
+        var mockEmailService = new Mock<IEmailService>();
+        var order = new Order(mockEmailService.Object, 25);
         var orderNumber = order.Submit();
         Assert.Equal(1, orderNumber);
+    }
+
+    [Fact]
+    public void EmailIsSent_WhenOrderIsSubmitted()
+    {
+        var mockEmailService = new Mock<IEmailService>();
+        
+        var order = new Order(mockEmailService.Object, 1);
+        order.Submit();
+        
+        mockEmailService.Verify(x => 
+            x.Send(It.Is<MailMessage>(mm => 
+                mm.To.Single().ToString() == "warehouseoperator@company.com" && 
+                mm.Subject == "1")), 
+            Times.Once);
     }
 }
